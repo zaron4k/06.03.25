f8df559e686aef3fa09abb858434fd61380451b8
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-03-06 11:08:51 +0200

    Add files via upload

---
+++ ConsoleApp6/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private double year;
+
+        public string Make
+        {
+            get { return make; }
+            set
+            {
+                make = value;
+            }
+        }
+        public string Model
+        {
+            get { return model; }
+            set
+            {
+                model = value;
+            }
+        }
+        public double Year
+        {
+            get { return year; }
+            set
+            {
+                if (value < 20)
+                    year = 2000;
+                else if (value > 120)
+                    year = 2000;
+                else year = value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Car()
+        { }
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="brond"></param>
+        public Car(string make, string model, double year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+        public override string ToString()
+        {
+            return $"Марка: {Make}\nМодель: {Model}\nГод: {Year} км/ч";
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "Lada";
+            car1.Model = "Infinity";
+            car1.Year = 1992;
+            Console.WriteLine($"{car1.ToString()}");
+            Console.WriteLine();
+            Car car2 = new Car("Lamba","Wentes",2000);
+            Console.WriteLine($"{car2.ToString()}");
+            Console.WriteLine();
+
+            Console.Write("Марка: ");
+            string make = Console.ReadLine();
+            Console.Write("Модель: ");
+            string model = Console.ReadLine();
+            Console.Write("Год: ");
+            double year = double.Parse(Console.ReadLine());
+            Car car3 = new Car(make, model, year);
+            Console.WriteLine($"{car3.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
