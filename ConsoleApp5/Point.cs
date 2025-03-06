89bbeceb0dd6d45e89cc7d7c14ee9725c6d4404b
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-03-06 11:09:30 +0200

    Add files via upload

---
+++ ConsoleApp5/Point.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Point
+    {
+        
+        private int x;
+        private int y;
+
+        
+        public double X
+        {
+            get { return x; }
+            set { x = (int)value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = (int)value; }
+        }
+
+        
+        public Point(double x, double y)
+        {
+            x = 0;
+            y = 0;
+        }
+
+      
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"({x},{y})"); 
+        }
+
+    }
+
+}
+
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = new int[4] { 5, 3, 1, -2 };
+            Console.WriteLine($""
+            //Point[] points = new Point[4];
+            //// Point[] points = new Point[]{
+            //new Point(3, 4);
+            //new Point(7, 4);
+            //new Point(7, 6);
+            //new Point(3, 6);
+            Console.ReadLine();
+        }
+    }
+    }
+
